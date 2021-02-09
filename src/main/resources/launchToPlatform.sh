e5e9f1d6469484f9d4879209f22fd73b55adc3a5
frisch <frisch@cloudera.com>
2021-02-09 09:34:57 +0100

    deploy(ssh): add possibility of ssh key

--- src/main/resources/launchToPlatform.sh
+++ src/main/resources/launchToPlatform.sh
@@ -2,28 +2,29 @@
 
 # Set an Hostname with below export to launch it
 # export HOST=
+# export SSH_KEY="-i <SSH_KEY>"
 
 export USER=root
 export DEST_DIR="/home/root/random-datagen"
 
 echo "Create needed directory on platform and send required files there"
 
-ssh ${USER}@${HOST} "mkdir -p ${DEST_DIR}/"
-ssh ${USER}@${HOST} "mkdir -p ${DEST_DIR}/resources/"
+ssh ${SSH_KEY} ${USER}@${HOST} "mkdir -p ${DEST_DIR}/"
+ssh ${SSH_KEY} ${USER}@${HOST} "mkdir -p ${DEST_DIR}/resources/"
 
-scp src/main/resources/models/*.json ${USER}@${HOST}:${DEST_DIR}/
-scp src/main/resources/dictionnaries/* ${USER}@${HOST}:${DEST_DIR}/
-scp src/main/resources/*.properties ${USER}@${HOST}:${DEST_DIR}/
-scp src/main/resources/launch.sh ${USER}@${HOST}:${DEST_DIR}/
+scp ${SSH_KEY} src/main/resources/models/*.json ${USER}@${HOST}:${DEST_DIR}/
+scp ${SSH_KEY} src/main/resources/dictionnaries/* ${USER}@${HOST}:${DEST_DIR}/
+scp ${SSH_KEY} src/main/resources/*.properties ${USER}@${HOST}:${DEST_DIR}/
+scp ${SSH_KEY} src/main/resources/launch.sh ${USER}@${HOST}:${DEST_DIR}/
 
-ssh ${USER}@${HOST} "chmod +x ${DEST_DIR}/launch.sh"
+ssh ${SSH_KEY} ${USER}@${HOST} "chmod +x ${DEST_DIR}/launch.sh"
 
-scp target/random-datagen-*.jar ${USER}@${HOST}:${DEST_DIR}/random-datagen.jar
+scp ${SSH_KEY} target/random-datagen-*.jar ${USER}@${HOST}:${DEST_DIR}/random-datagen.jar
 
 echo "Finished to send required files"
 
 echo "Launch script on platform to launch program properly"
-ssh ${USER}@${HOST} 'bash -s' < src/main/resources/launch.sh $@
+ssh ${SSH_KEY} ${USER}@${HOST} 'bash -s' < src/main/resources/launch.sh $@
 echo "Program finished"
 
 
