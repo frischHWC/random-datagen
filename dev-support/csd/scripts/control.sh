ff9eb0b3d30f3ee7528234c1300fd5aa9ca5be8d
frisch <frisch@cloudera.com>
2022-09-20 22:09:17 +0200

    test(properties): injection of conf_dir

--- dev-support/csd/scripts/control.sh
+++ dev-support/csd/scripts/control.sh
@@ -4,6 +4,10 @@ CMD=$1
 case $CMD in
   (start)
     echo "Starting DATAGEN"
+    echo $CONF_DIR
+    echo $(pwd)
+    envsubst < "${CONF_DIR}/service.properties" > "${CONF_DIR}/service.properties.tmp"
+    mv "${CONF_DIR}/service.properties.tmp" "${CONF_DIR}/service.properties"
     exec ${JAVA_HOME}/bin/java -jar -Dspring.profiles.active=cdp -Xmx${MAX_HEAP_SIZE}G ${DATAGEN_JAR_PATH}
     ;;
  (local_json)
