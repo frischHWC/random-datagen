1b330a5d7d92d8a4ac77baf3d3521831449cacca
frisch <frisch@cloudera.com>
2022-09-20 15:17:00 +0200

    deploy(csd): add properties and dynamic file

--- src/main/java/com/cloudera/frisch/randomdatagen/config/PropertiesLoader.java
+++ src/main/java/com/cloudera/frisch/randomdatagen/config/PropertiesLoader.java
@@ -1,12 +1,16 @@
 package com.cloudera.frisch.randomdatagen.config;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesLoader {
+
+    @Autowired
+    private static SpringConfig springConfig;
 
     private PropertiesLoader() { throw new IllegalStateException("Could not initialize this class"); }
 
@@ -17,9 +21,14 @@ public class PropertiesLoader {
         // Load config file
         java.util.Properties properties = new java.util.Properties();
 
+        String pathToApplicationProperties = "src/main/resources/application.properties";
+        if(springConfig.activeProfile.equalsIgnoreCase("cdp")) {
+            logger.info("Detected to be in cdp profile, so will load service.properties file");
+            pathToApplicationProperties = "service.properties";
+        }
+
         try {
-            FileInputStream fileInputStream = new FileInputStream(
-                "src/main/resources/application.properties");
+            FileInputStream fileInputStream = new FileInputStream(pathToApplicationProperties);
             properties.load(fileInputStream);
         } catch (IOException e) {
             logger.error("Property file not found !", e);
@@ -48,4 +57,14 @@ public class PropertiesLoader {
         logger.debug("For key: " + key + " returning property: " + property);
         return property;
     }
+
+    // TODO: At startup make Auto-Discovery: all important properties should be checked foreach service
+    /*
+    - If properties for a service are not set (ex: hdfs.uri) => Look for config file's location property
+    - If this is empty or file does not exists => WARNING: You should provide info on API call to use this sink
+    - Otherwise, load the file and set the required property (hdfs.uri for example)
+     */
+
+    // TODO: Foreach sink, a copy of the PropertiesLoader should be set and send to the sink,
+    //  so an API call can override some variables
 }
