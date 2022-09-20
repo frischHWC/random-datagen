5f4ea69cb293a3323d5b4f8cdbd02d5b58b66dfa
frisch <frisch@cloudera.com>
2022-09-20 16:44:33 +0200

    fix(csd): fix descriptor file$

--- src/main/java/com/cloudera/frisch/randomdatagen/config/PropertiesLoader.java
+++ src/main/java/com/cloudera/frisch/randomdatagen/config/PropertiesLoader.java
@@ -33,6 +33,9 @@ public class PropertiesLoader {
         } catch (IOException e) {
             logger.error("Property file not found !", e);
         }
+
+        autoDiscover();
+
         return properties;
     }
 
@@ -58,13 +61,74 @@ public class PropertiesLoader {
         return property;
     }
 
+
+    // TODO: Foreach sink, a copy of the PropertiesLoader should be set and send to the sink,
+    //  so an API call can override some variables
+    public static Properties getPropertiesCopy() {
+        return new Properties(properties);
+    }
+
     // TODO: At startup make Auto-Discovery: all important properties should be checked foreach service
     /*
     - If properties for a service are not set (ex: hdfs.uri) => Look for config file's location property
     - If this is empty or file does not exists => WARNING: You should provide info on API call to use this sink
     - Otherwise, load the file and set the required property (hdfs.uri for example)
      */
+    private static void autoDiscover() {
+        logger.info("Starting auto-discover of properties after load of properties file");
 
-    // TODO: Foreach sink, a copy of the PropertiesLoader should be set and send to the sink,
-    //  so an API call can override some variables
+        if(getProperty("hdfs.uri").isEmpty() && !getProperty("hadoop.core.site.path").isEmpty() && !getProperty("hadoop.hdfs.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hdfs.uri");
+        }
+
+        if(getProperty("hbase.zookeeper.quorum").isEmpty() && !getProperty("hadoop.hbase.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hbase.zookeeper.quorum");
+        }
+
+        if(getProperty("hbase.zookeeper.port").isEmpty() && !getProperty("hadoop.hbase.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hbase.zookeeper.port");
+        }
+
+        if(getProperty("hbase.zookeeper.znode").isEmpty() && !getProperty("hadoop.hbase.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hbase.zookeeper.znode");
+        }
+
+        if(getProperty("ozone.service.id").isEmpty() && !getProperty("hadoop.ozone.site.path").isEmpty()) {
+            logger.info("Going to auto-discover ozone.service.id");
+        }
+
+        if(getProperty("hive.zookeeper.quorum").isEmpty() && !getProperty("hadoop.hive.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hive.zookeeper.quorum");
+        }
+
+        if(getProperty("hive.zookeeper.znode").isEmpty() && !getProperty("hadoop.hive.site.path").isEmpty()) {
+            logger.info("Going to auto-discover hive.zookeeper.znode");
+        }
+
+        if(getProperty("solr.server.url").isEmpty() && !getProperty("solr.env.path").isEmpty()) {
+            logger.info("Going to auto-discover solr.server.url");
+        }
+
+        if(getProperty("solr.server.port").isEmpty() && !getProperty("solr.env.path").isEmpty()) {
+            logger.info("Going to auto-discover solr.server.port");
+        }
+
+        if(getProperty("kafka.brokers").isEmpty() && !getProperty("kafka.conf.client.path").isEmpty() && !getProperty("kafka.conf.cluster.path").isEmpty()) {
+            logger.info("Going to auto-discover kafka.brokers");
+        }
+
+        if(getProperty("kafka.security.protocol").isEmpty() && !getProperty("kafka.conf.client.path").isEmpty() && !getProperty("kafka.conf.cluster.path").isEmpty()) {
+            logger.info("Going to auto-discover kafka.security.protocol");
+        }
+
+        if(getProperty("schema.registry.url").isEmpty() && !getProperty("schema.registry.conf.path").isEmpty()) {
+            logger.info("Going to auto-discover schema.registry.url");
+        }
+
+        if(getProperty("kudu.master.server").isEmpty() && !getProperty("kudu.conf.path").isEmpty()) {
+            logger.info("Going to auto-discover kudu.master.server");
+        }
+
+    }
+
 }
