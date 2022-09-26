1cc7ad7056a4f333bdf7239a3e28d1028fdd02e1
frisch <frisch@cloudera.com>
2022-09-26 11:22:04 +0200

    fix(ranger): set well policiesÃ

--- dev-support/csd/scripts/init.sh
+++ dev-support/csd/scripts/init.sh
@@ -13,49 +13,49 @@ then
   if [ "${HBASE_SERVICE}" != "" ] && [ "${HBASE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to HBase"
-    curl -k -X POST -d "@scripts/policies/hbase.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/hbase.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${HDFS_SERVICE}" != "" ] && [ "${HDFS_SERVICE}" != "none" ]
   then
     echo "Pushing policy to HDFS"
-    curl -k -X POST -d "@scripts/policies/gdfs.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/hdfs.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${HIVE_SERVICE}" != "" ] && [ "${HIVE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Hive"
-    curl -k -X POST -d "@scripts/policies/hive.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/hive.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${KAFKA_SERVICE}" != "" ] && [ "${KAFKA_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Kafka"
-    curl -k -X POST -d "@scripts/policies/kafka.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/kafka.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${KUDU_SERVICE}" != "" ] && [ "${KUDU_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Kudu"
-    curl -k -X POST -d "@scripts/policies/kudu.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/kudu.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${OZONE_SERVICE}" != "" ] && [ "${OZONE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Ozone"
-    curl -k -X POST -d "@scripts/policies/ozone.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/ozone.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${SCHEMAREGISTRY_SERVICE}" != "" ] && [ "${SCHEMAREGISTRY_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Schema Registry"
-    curl -X POST -d "@scripts/policies/schemaregistry.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/schemaregistry.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${SOLR_SERVICE}" != "" ] && [ "${SOLR_SERVICE}" != "none" ]
   then
     echo "Pushing policy to SolR"
-    curl -k -X POST -d "@scripts/policies/solr.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -H "Content-Type: application/json" -H "Accept: application/json" -d "@scripts/policies/solr.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
 fi
