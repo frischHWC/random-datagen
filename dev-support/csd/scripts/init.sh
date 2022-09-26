659f002acbcb6b329a3e89da2a67b54af4cccc4d
frisch <frisch@cloudera.com>
2022-09-26 09:18:49 +0200

    tets(ranger): correct sdl

--- dev-support/csd/scripts/init.sh
+++ dev-support/csd/scripts/init.sh
@@ -5,11 +5,15 @@ echo "STARTING INIT OF DATAGEN"
 echo "Create local directory: /tmp/${DATAGEN_USER}"
 # Create local directories for data generation
 mkdir -p /tmp/${DATAGEN_USER}/
-chown datagen:datagen /tmp/${DATAGEN_USER}/
-chmod 755 /tmp/${DATAGEN_USER}/
+sudo chown datagen:datagen /tmp/${DATAGEN_USER}/
+sudo chmod 755 /tmp/${DATAGEN_USER}/
 
 echo "Finished to create local directory"
 
+echo "RANGER_SERVICE: ${RANGER_SERVICE}"
+echo "RANGER_URL: ${RANGER_URL}"
+echo "RANGER_ADMIN_USER: ${RANGER_ADMIN_USER}"
+echo "RANGER_ADMIN_PASSWORD: ${RANGER_ADMIN_PASSWORD}"
 
 if [ "${RANGER_SERVICE}" != "" ] && [ "${RANGER_SERVICE}" != "none" ] && [ "${RANGER_URL}" != "" ] && [ "${RANGER_ADMIN_USER}" != "" ] && [ "${RANGER_ADMIN_PASSWORD}" != "" ]
 then
