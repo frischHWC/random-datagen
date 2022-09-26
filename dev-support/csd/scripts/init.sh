1c935a7ab61792c0bca581e861a5b6af8d6546da
frisch <frisch@cloudera.com>
2022-09-26 10:22:03 +0200

    feat(ranger): set policies with -k

--- dev-support/csd/scripts/init.sh
+++ dev-support/csd/scripts/init.sh
@@ -5,7 +5,6 @@ echo "STARTING INIT OF DATAGEN"
 echo "RANGER_SERVICE: ${RANGER_SERVICE_NAME}"
 echo "RANGER_URL: ${RANGER_URL}"
 echo "RANGER_ADMIN_USER: ${RANGER_ADMIN_USER}"
-echo "RANGER_ADMIN_PASSWORD: ${RANGER_ADMIN_PASSWORD}"
 
 if [ "${RANGER_SERVICE_NAME}" != "" ] && [ "${RANGER_SERVICE_NAME}" != "none" ] && [ "${RANGER_URL}" != "" ] && [ "${RANGER_ADMIN_USER}" != "" ] && [ "${RANGER_ADMIN_PASSWORD}" != "" ]
 then
@@ -14,37 +13,37 @@ then
   if [ "${HBASE_SERVICE}" != "" ] && [ "${HBASE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to HBase"
-    curl -X POST -d "@scripts/policies/hbase.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/hbase.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${HDFS_SERVICE}" != "" ] && [ "${HDFS_SERVICE}" != "none" ]
   then
     echo "Pushing policy to HDFS"
-    curl -X POST -d "@scripts/policies/gdfs.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/gdfs.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${HIVE_SERVICE}" != "" ] && [ "${HIVE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Hive"
-    curl -X POST -d "@scripts/policies/hive.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/hive.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${KAFKA_SERVICE}" != "" ] && [ "${KAFKA_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Kafka"
-    curl -X POST -d "@scripts/policies/kafka.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/kafka.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${KUDU_SERVICE}" != "" ] && [ "${KUDU_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Kudu"
-    curl -X POST -d "@scripts/policies/kudu.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/kudu.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${OZONE_SERVICE}" != "" ] && [ "${OZONE_SERVICE}" != "none" ]
   then
     echo "Pushing policy to Ozone"
-    curl -X POST -d "@scripts/policies/ozone.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+    curl -k -X POST -d "@scripts/policies/ozone.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
   if [ "${SCHEMAREGISTRY_SERVICE}" != "" ] && [ "${SCHEMAREGISTRY_SERVICE}" != "none" ]
@@ -53,11 +52,11 @@ then
     curl -X POST -d "@scripts/policies/schemaregistry.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
   fi
 
-    if [ "${SOLR_SERVICE}" != "" ] && [ "${SOLR_SERVICE}" != "none" ]
-    then
-      echo "Pushing policy to SolR"
-      curl -X POST -d "@scripts/policies/solr.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
-    fi
+  if [ "${SOLR_SERVICE}" != "" ] && [ "${SOLR_SERVICE}" != "none" ]
+  then
+    echo "Pushing policy to SolR"
+    curl -k -X POST -d "@scripts/policies/solr.json" -u ${RANGER_ADMIN_USER}:${RANGER_ADMIN_PASSWORD}  ${RANGER_URL}/service/public/v2/api/policy
+  fi
 
 fi
 
